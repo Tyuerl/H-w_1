0ed0fdb35046d44150b8b739eb3a726f6efdd0da
Tyuerl <73366007+Tyuerl@users.noreply.github.com>
2020-11-14 16:38:18 +0300

    Update firstEx.cpp

--- firstEx.cpp
+++ firstEx.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
 	int a, b, c, d;
+	bool swth = 0;
 	cout << "Enter a = ";
 	cin >> a;
 	cout << "Enter b = ";
@@ -17,9 +18,14 @@ int main()
 	for (int i = 10000; i < 100000; i++)
 	{
 		if (i % a == b && i % c == d)
+		{
 			cout << i << " ";
+			swth = 1;
+		}
+			
 	}
 	cout << endl;
+	if (swth == 0) cout << "-1";
 	system("pause");
 	return 0;
 }
